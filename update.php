66c6f71448a90e1680c7571f4988403def8699ed
officialbektur <82287316+officialbektur@users.noreply.github.com>
2021-10-01 21:23:07 +0600

    ss

---
+++ update.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+//Обновление информации о продукте
+
+/*
+ * Подключаем файл для получения соединения к базе данных (PhpMyAdmin, MySQL)
+ */
+
+require_once '../config/connect.php';
+
+/*
+ * Создаем переменные со значениями, которые были получены с $_POST
+ */
+
+$id = $_POST['id'];
+$password = $_POST['password'];
+
+/*
+ * Делаем запрос на изменение строки в таблице products
+ */
+
+mysqli_query($connect, "UPDATE `passwords` SET `password` = '$password' WHERE `passwords`.`id` = '$id'");
+
+/*
+ * Переадресация на главную страницу
+ */
+
+header('Location: index.php');
+?>
