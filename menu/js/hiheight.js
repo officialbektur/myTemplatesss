b2bac66c793590fff4ff54327637a01642b4f2a0
officialbektur <82287316+officialbektur@users.noreply.github.com>
2021-10-07 23:25:25 +0600

    21

--- menu/js/hiheight.js
+++ menu/js/hiheight.js
@@ -1,8 +1,13 @@
 /* ====================  hi ImgBg  --Start--  ==================== */
 let hiImgBg = document.querySelector(".hi");
 let fullWindowInner = document.body.clientHeight || document.documentElement.clientHeight;
-let bgiMinusElementHeight = document.querySelector(".bgi-minus-height").offsetHeight;
-let windowInner = fullWindowInner - bgiMinusElementHeight;
+let bgiMinusElementHeight = document.querySelector(".bgi-minus-height");
+let windowInner;
+if (bgiMinusElementHeight) {
+	windowInner = fullWindowInner - bgiMinusElementHeight.offsetHeight;
+} else {
+	windowInner = fullWindowInner;
+}
 if (windowInner >= 1300) {
 	hiImgBg.style.height = 900 + "px";
 } else if(windowInner >= 500) {
